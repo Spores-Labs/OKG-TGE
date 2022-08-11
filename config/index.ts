39a9c6105c383713d32c4c04d86d04acf39a1540
DuyAnhTran95 <duyanh.tran8995@gmail.com>
2022-08-11 17:05:20 +0700

    remove unused config

--- config/index.ts
+++ config/index.ts
@@ -5,7 +5,6 @@ export const token = {
   name: 'Ookeenga',
   symbol: 'OKG',
   initialSupply: utils.parseEther('500000000'),
-  owner: '0x482bE3Ec3A24eE4FEc390576473dbc9DaD2E6d66',
 };
 
 export const upfronts = getUpfront();
